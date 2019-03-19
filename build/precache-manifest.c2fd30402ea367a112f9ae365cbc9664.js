612c38d8af386e86f11e33b0ea0113967a8a7e6f
JoLuMTavares <joaoluism.tavares@gmail.com>
2019-03-19 15:10:42 +0100

    More node modules (for github pages also)

---
+++ build/precache-manifest.c2fd30402ea367a112f9ae365cbc9664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ec4b14468adbcc6ddb0",
+    "url": "/Phone-Shop/static/css/main.6fe5f49f.chunk.css"
+  },
+  {
+    "revision": "3ec4b14468adbcc6ddb0",
+    "url": "/Phone-Shop/static/js/main.3ec4b144.chunk.js"
+  },
+  {
+    "revision": "b5a25a7f0331c795d7e8",
+    "url": "/Phone-Shop/static/css/1.6381138b.chunk.css"
+  },
+  {
+    "revision": "b5a25a7f0331c795d7e8",
+    "url": "/Phone-Shop/static/js/1.b5a25a7f.chunk.js"
+  },
+  {
+    "revision": "12f1966f7cd6220a2031",
+    "url": "/Phone-Shop/static/js/runtime~main.12f1966f.js"
+  },
+  {
+    "revision": "49ea0b0cc5329a1e683d0f1b0d64b5b3",
+    "url": "/Phone-Shop/index.html"
+  }
+];
